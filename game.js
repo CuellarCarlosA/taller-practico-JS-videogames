498a6e57a20945b764239758ab7ab0eedeaeed08
Carlos Cuellar <cc600142@gmail.com>
2023-04-19 16:34:24 -0600

    changesToGame

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame(){
+    //ame.fillRect(0,0,100,100);
+    //game.clearRect(0,0,50,50);
+    game.font = '25px Verdana';
+    game.fillStyle= 'purple';
+    game.textAlign='right';
+    game.fillText('Platzi',100,50)
+
+}
