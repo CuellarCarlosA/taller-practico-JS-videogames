0b626ccf5aad2764c4fd0aca49255c9cdf14b9d7
Carlos Cuellar <cc600142@gmail.com>
2023-04-20 11:44:01 -0600

    cambiosEnGame

--- game.js
+++ game.js
@@ -1,14 +1,33 @@
-const canvas = document.querySelector('#game');
-const game = canvas.getContext('2d');
+const canvas=document.querySelector('#game');
+const game=canvas.getContext('2d');
+window.addEventListener('load',setCanvasSize);
+window.addEventListener('resize',setCanvasSize);
+let canvasSize;
+let elementsSize;
 
-window.addEventListener('load', startGame);
+function setCanvasSize(){
+
+    if(window.innerHeight>window.innerWidth){
+        canvasSize=window.innerWidth*0.8;
+    }else{
+        canvasSize=window.innerHeight*0.8;
+    }
+    canvas.setAttribute('width',canvasSize);
+    canvas.setAttribute('height',canvasSize);
+
+    elementsSize= canvasSize/10;
+    startGame();
+}
 
 function startGame(){
-    //ame.fillRect(0,0,100,100);
-    //game.clearRect(0,0,50,50);
-    game.font = '25px Verdana';
-    game.fillStyle= 'purple';
-    game.textAlign='right';
-    game.fillText('Platzi',100,50)
 
+    console.log({canvasSize,elementsSize});
+
+    game.font=elementsSize+'px Verdana';
+    game.textAlign='end';
+
+    for(let i= 1;i<=10;i++){
+        game.fillText(emojis['X'],elementsSize,elementsSize*i);
+    }
 }
+
